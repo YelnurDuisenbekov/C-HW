33513458b2efbded26108d0dbdb8d677fc72f231
Yelnur <yelyadu@gmail.com>
2023-02-12 17:50:34 +0600

    Task2 done

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// 2 числа, большее и меньшее
+Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()!);
+if (number1 > number2)
+{
+    Console.WriteLine("Максимальное значение: " + number1);
+    Console.WriteLine("Минимальное значение: " + number2);
+}
+else
+{
+    Console.WriteLine("Максимальное значение: " + number2);
+    Console.WriteLine("Минимальное значение: " + number1);
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()!);
+if (number1 > number2)
+{
+    Console.WriteLine("Максимальное значение: " + number1);
+    Console.WriteLine("Минимальное значение: " + number2);
+}
+else
+{
+    Console.WriteLine("Максимальное значение: " + number2);
+    Console.WriteLine("Минимальное значение: " + number1);
+}
