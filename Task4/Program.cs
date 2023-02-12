8f1c4af2e0da7469a2ce8ff8b414b1a75eff40e5
Yelnur <yelyadu@gmail.com>
2023-02-12 18:19:00 +0600

    Task4 done

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -2,13 +2,26 @@
 int number1 = int.Parse(Console.ReadLine()!);
 Console.Write("Введите второе число: ");
 int number2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье число: ");
+int number3 = int.Parse(Console.ReadLine()!);
 if (number1 > number2)
 {
+    if (number1 > number3)
+    {
     Console.WriteLine("Максимальное значение: " + number1);
-    Console.WriteLine("Минимальное значение: " + number2);
+    }
 }
-else
+if (number2 > number1)
 {
+    if (number2 > number3)
+    {
     Console.WriteLine("Максимальное значение: " + number2);
-    Console.WriteLine("Минимальное значение: " + number1);
+    }
+}
+if (number3 > number2)
+{
+    if (number3 > number1)
+    {
+    Console.WriteLine("Максимальное значение: " + number3);
+    }
 }
