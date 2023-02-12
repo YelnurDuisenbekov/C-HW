2ca752d1558ec078b789e04c2bc5fee4b95a7323
Yelnur <yelyadu@gmail.com>
2023-02-12 18:43:03 +0600

    Task8 done

---
+++ Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿// все четные до n
+Console.Write("Введите число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+int i;
+for (i =1; i < number1; i++)
+{
+    if (i % 2 == 0)
+    {
+       Console.Write(i);
+    }
+}
